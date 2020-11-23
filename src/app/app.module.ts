c899e2738306f689a5274f9cfa3408c5ed37d889
Leonardo Favio Romero Silva <leo@MacBook-Pro-de-Leonardo.local>
2020-11-22 19:14:13 -0500

    Se agrega formulario para el ingreso de datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParticipantComponent } from './component/participant/participant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParticipantComponent
   ],
   imports: [
     BrowserModule,
