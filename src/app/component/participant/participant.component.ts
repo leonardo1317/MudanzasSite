4b8b240b9c361e9b63335b9a071c3d924a67f4a6
Leonardo Favio Romero Silva <leo@MacBook-Pro-de-Leonardo.local>
2020-11-23 18:49:01 -0500

    actualizacion

--- src/app/component/participant/participant.component.ts
+++ src/app/component/participant/participant.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { Participant } from 'src/app/domain/participant';
 import { ParticipantService } from 'src/app/service/participant.service';
@@ -10,11 +11,16 @@ import { ParticipantService } from 'src/app/service/participant.service';
   styleUrls: ['./participant.component.css']
 })
 export class ParticipantComponent implements OnInit, OnDestroy {
-  public participantId: string='';
+  formGroup: FormGroup;
   public participants: Participant[];
   public fileToUpload: any;
   private sud: Subscription;
-  constructor(public participantService: ParticipantService) { }
+  constructor(public participantService: ParticipantService, private builder:FormBuilder) {
+    this.formGroup = this.builder.group({
+      'participantId': ['',Validators.required],
+      'file':['',Validators.required]
+    })
+   }
 
   ngOnInit(): void {
     this.reset();
@@ -30,11 +36,15 @@ export class ParticipantComponent implements OnInit, OnDestroy {
 
   handleFileInput(files: FileList) {
     this.fileToUpload = files.item(0);
-}
+  }
 
-  save(): void{
-  
-      this.participantService.save(this.participantId,  this.fileToUpload).subscribe(ok => {
+  save(values: any): void{
+
+    let participantId = values.participantId;
+    if(this.isEmpty(participantId) || this.isEmpty(this.fileToUpload)){
+      return;
+    }
+     this.participantService.save(participantId,  this.fileToUpload).subscribe(ok => {
         this.reset();
         this.findAll();
     }, err => {
@@ -51,7 +61,7 @@ export class ParticipantComponent implements OnInit, OnDestroy {
     });
   }
 
-  download(trace:string){
+  download(trace:string):void{
     let pom = document.createElement('a');
     pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(trace));
     pom.setAttribute('download', "lazy_loading_example_output.txt");
@@ -67,9 +77,14 @@ export class ParticipantComponent implements OnInit, OnDestroy {
 
   }
 
-  reset() {
-    this.participantId = '';
-}
+  reset():void { 
+    this.formGroup.reset();
+    this.fileToUpload = '';
+  }
+
+  isEmpty(value: string):boolean{
+    return (value == null || value.length === 0);
+  }
 
 
   ngOnDestroy(): void {
